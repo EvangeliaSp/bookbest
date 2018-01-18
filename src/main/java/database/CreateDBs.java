c62b60e0f5ed9844ab5308d43c5df2dd4c715480
EvangeliaSp <vagspax@gmail.com>
2018-01-18 16:49:06 +0200

    File that includes mappings from dbs to the ontology

--- src/main/java/database/CreateDBs.java
+++ src/main/java/database/CreateDBs.java
@@ -2,7 +2,6 @@ package database;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -21,20 +20,30 @@ public class CreateDBs {
         Class.forName(this.jdbcDriver);
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/?user=root&password=5698");
         Statement s = conn.createStatement();
-        int Result = s.executeUpdate("CREATE DATABASE IF NOT EXISTS "+database+"e;");
+        int Result = s.executeUpdate("CREATE DATABASE IF NOT EXISTS "+database+";");
         System.out.println(database);
     }
 
-    private void createTable(Connection connection, Statement statement, String table, String columns) throws SQLException {
-        System.out.println(table);
+    private String createTable(Statement statement, String table, String columns) throws SQLException {
         int k = columns.indexOf(" ");
         String key = columns.substring(0, k);
+        String sqlCreate = "CREATE TABLE " + table +"(" +
+                columns +
+                " PRIMARY KEY ("+key+"))";
 
-        String sqlCreate = "CREATE TABLE IF NOT EXISTS " + table +" ("+
-                columns+", PRIMARY KEY (`" + key + "`))";
+        statement.executeUpdate(sqlCreate);
+        return key;
+    }
 
-        Statement stmt = connection.createStatement();
-        stmt.execute(sqlCreate);
+    private void createTable2(Statement statement, String table, String columns, String fkey, String ftable) throws SQLException {
+        int k = columns.indexOf(" ");
+        String key = columns.substring(0, k);
+        String sqlCreate = "CREATE TABLE " + table +"(" +
+                columns +
+                " PRIMARY KEY ("+key+"), "+
+                " FOREIGN KEY ("+key+") REFERENCES "+ftable+" ("+fkey+"))";
+        System.out.println(sqlCreate);
+        statement.executeUpdate(sqlCreate);
     }
 
     public void createDatabases() {
@@ -42,34 +51,28 @@ public class CreateDBs {
         try {
             FileReader fileReader = new FileReader(this.filename);
             BufferedReader br = new BufferedReader(fileReader);
-            String currentLine, database, table;
+            String currentLine, database, table, table2, key;
             DBConnection dbConnection = new DBConnection();
             Statement statement;
-            Connection connection;
 
             while ((currentLine = br.readLine()) != null) {
                 // Create Database
                 database = currentLine;
                 this.createDB(database);
                 dbConnection.connect(database);
-                connection = dbConnection.getConnection();
                 statement = dbConnection.getStatement();
 
                 if((currentLine = br.readLine()) != null) {
                     table = currentLine;
                     if((currentLine = br.readLine()) != null) {
-                        this.createTable(connection, statement, table, currentLine);
+                        key = this.createTable(statement, table, currentLine);
+                        if((currentLine = br.readLine()) != null) {
+                            table2 = currentLine;
+                            if((currentLine = br.readLine()) != null) {
+                                this.createTable2(statement, table2, currentLine, key, table);
+                            }
+                        }
                     }
-
-                }
-                if((currentLine = br.readLine()) != null) {
-
-                }
-                if((currentLine = br.readLine()) != null) {
-                    //this.createTable(currentLine);
-                }
-                if((currentLine = br.readLine()) != null) {
-
                 }
             }
 
